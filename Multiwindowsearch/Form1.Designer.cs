3ce84bc328036ac6767bf4de70c79287bc23fe13
space0215 <space0215@naver.com>
2020-12-16 17:26:45 +0900

    트리 컨트롤 기본 동작 추가

--- Multiwindowsearch/Form1.Designer.cs
+++ Multiwindowsearch/Form1.Designer.cs
@@ -34,7 +34,23 @@
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.treeView2 = new System.Windows.Forms.TreeView();
+            this.listView2 = new System.Windows.Forms.ListView();
             this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
             this.SuspendLayout();
             // 
             // imageList1
@@ -59,9 +75,11 @@
             this.tabControl1.SelectedIndex = 0;
             this.tabControl1.Size = new System.Drawing.Size(1061, 718);
             this.tabControl1.TabIndex = 0;
+            this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.splitContainer1);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
@@ -72,6 +90,7 @@
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.splitContainer2);
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
@@ -79,6 +98,78 @@
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(3, 3);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listView1);
+            this.splitContainer1.Size = new System.Drawing.Size(1047, 686);
+            this.splitContainer1.SplitterDistance = 349;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(349, 686);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView1_BeforeExpand);
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(694, 686);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(3, 3);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.treeView2);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.listView2);
+            this.splitContainer2.Size = new System.Drawing.Size(1047, 686);
+            this.splitContainer2.SplitterDistance = 349;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // treeView2
+            // 
+            this.treeView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView2.Location = new System.Drawing.Point(0, 0);
+            this.treeView2.Name = "treeView2";
+            this.treeView2.Size = new System.Drawing.Size(349, 686);
+            this.treeView2.TabIndex = 0;
+            this.treeView2.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView2_BeforeExpand);
+            // 
+            // listView2
+            // 
+            this.listView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(0, 0);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(694, 686);
+            this.listView2.TabIndex = 0;
+            this.listView2.UseCompatibleStateImageBehavior = false;
             // 
             // Form1
             // 
@@ -89,6 +180,16 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -98,6 +199,12 @@
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.TreeView treeView2;
+        private System.Windows.Forms.ListView listView2;
     }
 }
 
--- Multiwindowsearch/Form1.cs
+++ Multiwindowsearch/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,13 @@ namespace Multiwindowsearch
 {
     public partial class Form1 : Form
     {
+        public object TreeViewItem { get; private set; }
+
         public Form1()
         {
             InitializeComponent();
+            showTreeView(0);
+            showTreeView(1);
 
         }
 
@@ -22,6 +27,85 @@ namespace Multiwindowsearch
         {
                         
 
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int nSelectNo = tabControl1.SelectedIndex;
+        }
+
+
+        public void showTreeView(int nChannel)
+        {
+            TreeNode root;
+            if (nChannel == 0)
+            {
+                treeView1.Nodes.Clear();
+                root = treeView1.Nodes.Add("Root");
+            }
+            else
+            {
+                treeView2.Nodes.Clear();
+                root = treeView2.Nodes.Add("Root");
+            }
+            foreach (string drive in Directory.GetLogicalDrives())
+            {
+                DriveInfo di = new DriveInfo(drive);
+
+                if (di.IsReady)
+                {
+                    TreeNode node = root.Nodes.Add(drive);
+                    node.Nodes.Add("\\");
+                }
+
+            }
+        }
+
+        private void treeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            try
+            {
+                if (e.Node.Nodes.Count == 1 && e.Node.Nodes[0].Text.Equals("\\"))
+                {
+                    e.Node.Nodes.Clear();
+                    string path = e.Node.FullPath.Substring(e.Node.FullPath.IndexOf("\\") + 1);
+                    string[] directories = Directory.GetDirectories(path);
+
+                    foreach (string str in directories)
+                    {
+                        TreeNode newNode = e.Node.Nodes.Add(str.Substring(str.LastIndexOf("\\") + 1));
+                        newNode.Nodes.Add("\\");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Treeview1" + ex.Message);
+            }
+        }
+
+        private void treeView2_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            try
+            {
+                if (e.Node.Nodes.Count == 1 && e.Node.Nodes[0].Text.Equals("\\"))
+                {
+                    e.Node.Nodes.Clear();
+                    string path = e.Node.FullPath.Substring(e.Node.FullPath.IndexOf("\\") + 1);
+                    string[] directories = Directory.GetDirectories(path);
+
+
+                    foreach (string str in directories)
+                    {
+                        TreeNode  newNode = e.Node.Nodes.Add(str.Substring(str.LastIndexOf("\\") + 1));
+                        newNode.Nodes.Add("\\");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Treeview2" + ex.Message);
+            }
         }
     }
 
